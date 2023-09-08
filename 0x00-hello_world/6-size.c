dbab99c95976c41b97cbac7f60246181e81571b4
Albertbassey <aniedionetim@gmail.com>
2023-09-07 22:28:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,12 @@
 */
 int main(void)
 {
-        char acharacter;
-	int aninteger;
-	long along;
-	long long alonglong;
-	float afloat;
-	
+char acharacter;
+int aninteger;
+long along;
+long long alonglong;
+float afloat;
+
 printf("Size of a char: %d byte(s)\n", sizeof(acharacter));
 printf("Size of an int: %d byte(s)\n", sizeof(aninteger));
 printf("Size of a long int: %d byte(s)\n", sizeof(along));
