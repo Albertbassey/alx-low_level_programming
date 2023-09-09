c142d189221629677e68c40a207a2f71d2579c44
Albertbassey <aniedionetim@gmail.com>
2023-09-08 22:31:19 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-main - Entry point
+* main - Entry point
 * Return: always 0
 */
 int main(void)
@@ -12,7 +12,8 @@ putchar(n + '0');
 if (n < 9)
 {
 putchar(',');
-putchar(' ');				 				        }
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
