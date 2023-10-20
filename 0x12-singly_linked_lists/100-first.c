3ebdc95f92eb7304fc42bf804e4c0ec3ce6e629b
Albertbassey <aniediongetim@gmail.com>
2023-10-19 17:53:52 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,12 +4,6 @@
  * exec_before_main - print before main.
  */
 
-void exec_before_main(void) __attribute__((constructor));
-
-/**
- * exec_before_main - print before main.
- */
-
 void exec_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
