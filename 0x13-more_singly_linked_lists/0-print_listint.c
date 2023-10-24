165fad62ad09ff07a3975644f581389d786bbed9
Albertbassey <aniediongetim@gmail.com>
2023-10-24 11:48:23 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t j = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		i++;
+		j++;
 		h = h->next;
 	}
-	return (i);
+	return (j);
 }
